aadbe680edcb0aa9c9347d98167c4367515f7509
Denis Yakovlev <de1m0s242@gmail.com>
2020-06-27 19:39:44 +0700

    first attempt second case

--- src/main/kotlin/net/de1mos/alfabattle/macdonaldsnumber3/controllers/BranchController.kt
+++ src/main/kotlin/net/de1mos/alfabattle/macdonaldsnumber3/controllers/BranchController.kt
@@ -1,15 +1,15 @@
 package net.de1mos.alfabattle.macdonaldsnumber3.controllers
 
+import net.de1mos.alfabattle.macdonaldsnumber3.repositories.BranchDist
 import net.de1mos.alfabattle.macdonaldsnumber3.repositories.BranchRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import kotlin.math.roundToLong
 
 data class GetBranchByIdResponse(val id: Int, val title: String?, val lon: Float?, val lat: Float?, val address: String?)
 data class NotFoundResponse(val status: String)
+data class NearestBranchResponse(val id: Int, val title: String?, val lon: Float?, val lat: Float?, val address: String?, val distance: Long)
 
 @RestController
 @RequestMapping("/branches")
@@ -22,5 +22,13 @@ class BranchController(private val branchRepository: BranchRepository) {
             branch.isPresent -> ResponseEntity.ok(branch.map { GetBranchByIdResponse(it.id, it.title, it.lon, it.lat, it.address) })
             else -> ResponseEntity(NotFoundResponse("branch not found"), HttpStatus.NOT_FOUND)
         }
+    }
+
+    @GetMapping("")
+    fun nearest(@RequestParam("lat") lat: Float, @RequestParam("lon") lon: Float): NearestBranchResponse {
+        val nearest = branchRepository.findNearest(lat, lon)
+        val d = nearest.first()
+        val branch = d.branch
+        return NearestBranchResponse(branch.id, branch.title, branch.lon, branch.lat, branch.address, d.dist.roundToLong())
     }
 }
--- src/main/kotlin/net/de1mos/alfabattle/macdonaldsnumber3/repositories/BranchRepository.kt
+++ src/main/kotlin/net/de1mos/alfabattle/macdonaldsnumber3/repositories/BranchRepository.kt
@@ -1,9 +1,24 @@
 package net.de1mos.alfabattle.macdonaldsnumber3.repositories
 
 import net.de1mos.alfabattle.macdonaldsnumber3.models.Branch
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
+interface BranchDist {
+    val branch: Branch
+    val dist: Double
+}
+
 @Repository
 interface BranchRepository : CrudRepository<Branch, Int> {
+
+    @Query("""
+            select br as branch, sqrt(pow(69.1 * (br.lat - :lat), 2) + 
+                                pow(69.1 * (:lon - br.lon) * cos(br.lat / 57.3), 2)) as dist 
+            from Branch br
+            order by dist
+            
+    """)
+    fun findNearest(lat: Float, lon: Float): List<BranchDist>
 }
